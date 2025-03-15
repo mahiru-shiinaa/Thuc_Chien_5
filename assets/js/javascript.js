1defc0217c536427a281d5f8833838662bd1d6a4
Mahiru Shiina <halovemusics@gmail.com>
2025-03-15 20:28:47 +0700

    Version 1.3 feature-1

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -26,3 +26,65 @@ var swiper = new Swiper(".mySwiper", {
         $('header').removeClass('header-fixed');
     }
 });
+
+// Lưu thông tin căn hộ
+const properties = {
+  apartment: {
+    space: "185 m2",
+    floor: "26th",
+    rooms: 4,
+    parking: "Yes",
+    payment: "Bank",
+    image: "assets/images/deal-01.jpg", // Thay bằng đường dẫn ảnh thật
+    title: "Extra Info About Property",
+    description_1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor pack incididunt ut labore et dolore magna aliqua quised ipsum suspendisse.",
+    description_2: "When you need free CSS templates, you can simply type TemplateMo in any search engine website. In addition, you can type TemplateMo Portfolio, TemplateMo One Page Layouts, etc.",
+  },
+  villa: {
+      space: "250 m2",
+    floor: "28th",
+    rooms: 5,
+    parking: "Yes",
+    payment: "Bank",
+    image: "assets/images/deal-02.jpg", // Thay bằng đường dẫn ảnh thật
+    title: "Detail Info About Villa",
+    description_1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor pack incididunt ut labore et dolore magna aliqua quised ipsum suspendisse.",
+    description_2: "Swag fanny pack lyft blog twee. JOMO ethical copper mug, succulents typewriter shaman DIY kitsch twee taiyaki fixie hella venmo after messenger poutine next level humblebrag swag franzen.",
+  },
+  penthouse: {
+    space: "320 m2",
+    floor: "34th",
+    rooms: 6,
+    parking: "Yes",
+    payment: "Bank",
+    image: "assets/images/deal-03.jpg", // Thay bằng đường dẫn ảnh thật
+    title: "Extra Info About Penthouse",
+    description_1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor pack incididunt ut labore et dolore magna aliqua quised ipsum suspendisse.",
+    description_2: "Swag fanny pack lyft blog twee. JOMO ethical copper mug, succulents typewriter shaman DIY kitsch twee taiyaki fixie hella venmo after messenger poutine next level humblebrag swag franzen.",
+  },
+};
+
+//Hàm để set thông tin căn hộ
+function showProperty(propertyType) {
+    const property = properties[propertyType];
+
+    document.getElementById("space").textContent = property.space;
+    document.getElementById("floor").textContent = property.floor;
+    document.getElementById("rooms").textContent = property.rooms;
+    document.getElementById("parking").textContent = property.parking;
+    document.getElementById("payment").textContent = property.payment;
+    document.getElementById("image-detail").src = property.image; 
+    document.getElementById("detail-title").textContent = property.title; 
+    document.getElementById("detail-desc-1").textContent = property.description_1; 
+    document.getElementById("detail-desc-2").textContent = property.description_2;
+
+    const buttons = document.querySelectorAll(".button-group .button-three");
+    buttons.forEach(button => button.classList.remove("active"));
+    const activeButton = document.querySelector(`.button-group .button-three[onclick="showProperty('${propertyType}')"]`);
+
+    if (activeButton) {
+      activeButton.classList.add('active');
+  }
+}
+
+showProperty('apartment');
